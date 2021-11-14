7820587c33e916c4a584a9423e434ca5199c99e4
Elizabeth Christman <elizabethc99@vt.edu>
2021-11-14 11:40:05 -0500

    detect collision of cells

--- 2D-Jupyter.js
+++ 2D-Jupyter.js
@@ -149,10 +149,8 @@ define([  //dependencies
                         var cellSpatial = cells[i].metadata.spatial;
                         if(thisSpatial.left > cellSpatial.left && thisSpatial.top > cellSpatial.top){ //if collision
                             console.log("collision");
-                            //create div? 
-                            
-                           
-                            
+                            //make both cells into a single div object
+
                         }
                     }
                 }
@@ -447,27 +445,6 @@ define([  //dependencies
           }
 
         return this;
-    };
-
-    var is_colliding = function($div1, $div2) {
-        // Div 1 data
-        var d1_offset             = $div1.offset();
-        var d1_height             = $div1.outerHeight( true );
-        var d1_width              = $div1.outerWidth( true );
-        var d1_distance_from_top  = d1_offset.top + d1_height;
-        var d1_distance_from_left = d1_offset.left + d1_width;
-    
-        // Div 2 data
-        var d2_offset             = $div2.offset();
-        var d2_height             = $div2.outerHeight( true );
-        var d2_width              = $div2.outerWidth( true );
-        var d2_distance_from_top  = d2_offset.top + d2_height;
-        var d2_distance_from_left = d2_offset.left + d2_width;
-    
-        var not_colliding = ( d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left );
-    
-        // Return whether it IS colliding
-        return ! not_colliding;
     };
 
 
